70a8b63a530e41c5f2a02aa7ec02ca618418d371
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-17 12:21:12 +0900

    Update and rename [List] max_by_based_on_func.py to [List] max_min__based_on_func.py

---
+++ 30_seconds_of_code/[List] max_min__based_on_func.py
@@ -0,0 +1,38 @@
+
+'''
+1) max_by
+Returns the maximum value of a list,
+after mapping each element to a value using the provided function
+
+- Use map() with fn to map each element to a value using the provided function
+- Use max() to return the maximum value
+
+https://www.30secondsofcode.org/python/s/max-by
+'''
+
+def max_by(lst, fn):
+    return max(map(fn, lst))
+
+
+# Example
+max_by([{ 'n': 4 }, { 'n': 2 }, { 'n': 8 }, { 'n': 6 }], lambda v : v['n']) # 8
+
+
+'''
+2) min_by
+Returns the minimum value of a list, after mapping
+each element to a value using the provided function
+
+- Use map() with fn to map each element to a value using the provided function.
+- Use min() to return the minimum value.
+
+https://www.30secondsofcode.org/python/s/min-by
+'''
+
+def min_by(lst, fn):
+    return min(map(fn, lst))
+
+
+# Example
+min_by([{ 'n': 4 }, { 'n': 2 }, { 'n': 8 }, { 'n': 6 }], lambda v : v['n']) # 2
+
