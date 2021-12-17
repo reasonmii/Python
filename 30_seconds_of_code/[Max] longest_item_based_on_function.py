a2c21ae53e64e7bc0ff90ff3eeb6fed1d52f3ec3
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-17 11:47:05 +0900

    Update [Max] longest_item_based_on_function.py

--- 30_seconds_of_code/[Max] longest_item_based_on_function.py
+++ 30_seconds_of_code/[Max] longest_item_based_on_function.py
@@ -4,8 +4,8 @@ longest_item
 Takes any number of iterable objects or objects with a length property
 and returns the longest one
 
-- Use max() with len() as the key to return the item with the greatest length.
-- If multiple items have the same length, the first one will be returned.
+- Use max() with len() as the key to return the item with the greatest length
+- If multiple items have the same length, the first one will be returned
 
 https://www.30secondsofcode.org/python/s/longest-item
 '''
