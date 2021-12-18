94189899fe162793ef8f5f9bf5f3598236a0208b
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-18 17:54:01 +0900

    Update and rename [List] apply_func.py to [List] iter_apply_func.py

--- 30_seconds_of_code/[List] iter_apply_func.py
+++ 30_seconds_of_code/[List] iter_apply_func.py
@@ -1,5 +1,23 @@
 '''
-for_each_right
+1) for_each
+Executes the provided function once for each list element
+
+- Use a for loop to execute fn for each element in itr
+
+https://www.30secondsofcode.org/python/s/for-each
+'''
+
+def for_each(itr, fn):
+    for el in itr:
+        fn(el)   
+
+            
+# Example
+for_each([1, 2, 3], print) # 1 2 3
+
+
+'''
+2) for_each_right
 Executes the provided function once for each list element,
 starting from the list's last element
 
