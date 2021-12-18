c12f83a1fe3be24b4b1810ff8099d6e49c67e47b
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-18 16:56:13 +0900

    Create [List, Max] return_n_elements.py

---
+++ 30_seconds_of_code/[List, Max] return_n_elements.py
@@ -0,0 +1,22 @@
+
+'''
+max_n
+Returns the n maximum elements from the provided list
+
+- Use sorted() to sort the list
+- Use slice notation to get the specified number of elements
+- Omit the second argument, n, to get a one-element list.
+- If n is greater than or equal to the provided list's length,
+  then return the original list (sorted in descending order)
+
+https://www.30secondsofcode.org/python/s/max-n
+'''
+
+def max_n(lst, n=1):
+    return sorted(lst, reverse=True)[:n]
+
+            
+# Example
+max_n([1, 2, 3]) # [3]
+max_n([1, 2, 3], 2) # [3, 2]
+max_n([1, 2, 3], 4) # [3, 2]
