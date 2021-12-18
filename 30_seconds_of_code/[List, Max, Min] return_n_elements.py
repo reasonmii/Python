2904fe67b5a084917b35107e07c43a25b3a4f09a
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-18 16:57:36 +0900

    Update and rename [List, Max] return_n_elements.py to [List, Max, Min] return_n_elements.py

--- 30_seconds_of_code/[List, Max, Min] return_n_elements.py
+++ 30_seconds_of_code/[List, Max, Min] return_n_elements.py
@@ -20,3 +20,25 @@ def max_n(lst, n=1):
 max_n([1, 2, 3]) # [3]
 max_n([1, 2, 3], 2) # [3, 2]
 max_n([1, 2, 3], 4) # [3, 2]
+
+
+'''
+min_n
+Returns the n minimum elements from the provided list
+
+- Use sorted() to sort the list.
+- Use slice notation to get the specified number of elements
+- Omit the second argument, n, to get a one-element list
+- If n is greater than or equal to the provided list's length,
+  then return the original list (sorted in ascending order)
+
+https://www.30secondsofcode.org/python/s/min-n
+'''
+
+def min_n(lst, n=1):
+    return sorted(lst)[:n]
+
+            
+# Example
+min_n([1, 2, 3]) # [1]
+min_n([1, 2, 3], 2) # [1, 2]
