ba25f3387713b712fee480e7e163f16756282b5a
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-20 11:14:34 +0900

    Create [Math] covert_decimal_to_hexadecimal.py

---
+++ 30_seconds_of_code/[Math] covert_decimal_to_hexadecimal.py
@@ -0,0 +1,18 @@
+
+'''
+to_hex
+Returns the hexadecimal representation of the given number
+
+- Use hex() to convert a given decimal number into its hexadecimal equivalent
+
+https://www.30secondsofcode.org/python/s/fibonacci
+'''
+
+def to_hex(n):
+    return hex(n)
+
+
+# Example
+to_hex(41) # 0x29
+to_hex(332) # 0x14c
+
