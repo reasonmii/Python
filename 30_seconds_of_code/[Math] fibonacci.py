340c6dc25bbbfcd71c9bc0480a745269cf4c4009
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-20 11:15:26 +0900

    Update [Math] fibonacci.py

--- 30_seconds_of_code/[Math] fibonacci.py
+++ 30_seconds_of_code/[Math] fibonacci.py
@@ -3,8 +3,9 @@
 fibonacci
 Generates a list, containing the Fibonacci sequence, up until the nth term
 
-- Starting with 0 and 1, use list.append() to add the sum of the last two numbers of the list to the end of the list, until the length of the list reaches n.
-- If n is less or equal to 0, return a list containing 0.
+- Starting with 0 and 1, use list.append() to add the sum of the last two numbers of the list
+  to the end of the list, until the length of the list reaches n
+- If n is less or equal to 0, return a list containing 0
 
 https://www.30secondsofcode.org/python/s/fibonacci
 '''
