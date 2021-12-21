a9f4d403c3a1a4615e4cfe44d265201425148cda
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-21 14:41:40 +0900

    Update [Regexp] capitalize_1st_letter.py

--- 30_seconds_of_code/[Regexp] capitalize_1st_letter.py
+++ 30_seconds_of_code/[Regexp] capitalize_1st_letter.py
@@ -1,6 +1,6 @@
 
 '''
-capitalize
+1) capitalize
 Capitalizes the first letter of a string
 
 - Use list slicing and str.upper() to capitalize the first letter of the string
@@ -26,3 +26,23 @@ capitalize('fooBar', True) # 'Foobar'
 s = 'fooBar'
 s[:1]   # f
 
+
+'''
+2) decapitalize
+Decapitalizes the first letter of a string
+
+- Use list slicing and str.lower() to decapitalize the first letter of the string
+- Use str.join() to combine the lowercase first letter with the rest of the characters
+- Omit the upper_rest parameter to keep the rest of the string intact,
+  or set it to True to convert to uppercase
+  
+https://www.30secondsofcode.org/python/s/decapitalize
+'''
+
+def decapitalize(s, upper_rest=False):
+    return ''.join([s[:1].lower(), (s[1:].upper() if upper_rest else s[1:])])
+
+
+# Example
+decapitalize('FooBar') # 'fooBar'
+decapitalize('FooBar', True) # 'fOOBAR'
