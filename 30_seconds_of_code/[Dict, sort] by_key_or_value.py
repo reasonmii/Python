82fb15120be69eda58d5cdb266a0901f7ce58c50
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-21 14:47:12 +0900

    Update [Dict, sort] by_key_or_value.py

--- 30_seconds_of_code/[Dict, sort] by_key_or_value.py
+++ 30_seconds_of_code/[Dict, sort] by_key_or_value.py
@@ -23,3 +23,29 @@ sort_dict_by_key(d)
 
 sort_dict_by_key(d, True)
 # {'two': 2, 'three': 3, 'one': 1, 'four': 4, 'five': 5}
+
+
+'''
+2) sort_dict_by_value
+Sorts the given dictionary by value
+
+- Use dict.items() to get a list of tuple pairs from d and sort it using a lambda function and sorted()
+- Use dict() to convert the sorted list back to a dictionary
+- Use the reverse parameter in sorted() to sort the dictionary in reverse order, based on the second argument
+- ⚠️ NOTICE: Dictionary values must be of the same type
+
+https://www.30secondsofcode.org/python/s/sort-dict-by-value
+'''
+
+def sort_dict_by_value(d, reverse=False):
+    return dict(sorted(d.items(), key=lambda x: x[1], reverse=reverse))
+    
+
+# Example
+d = {'one': 1, 'three': 3, 'five': 5, 'two': 2, 'four': 4}
+
+sort_dict_by_value(d)
+# {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
+
+sort_dict_by_value(d, True)
+# {'five': 5, 'four': 4, 'three': 3, 'two': 2, 'one': 1}
