4c31d1bca7a2b72d37d1dde392e016bb41e9e727
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-21 14:43:56 +0900

    Update [Dictionary] find_key.py

--- 30_seconds_of_code/[Dictionary] find_key.py
+++ 30_seconds_of_code/[Dictionary] find_key.py
@@ -1,5 +1,5 @@
 '''
-find_key
+1) find_key
 Finds the first key in the provided dictionary that has the given value
 
 - Use dictionary.items() and next() to return the first key
@@ -20,3 +20,27 @@ ages = {
 }
 
 find_key(ages, 11) # 'Isabel'
+
+
+'''
+2) find_keys
+Finds all keys in the provided dictionary that have the given value
+
+- Use dictionary.items(), a generator and list() to return all keys
+  that have a value equal to val
+
+https://www.30secondsofcode.org/python/s/find-keys
+'''
+
+def find_keys(dic, val):
+    return list(key for key, v in dic.items() if v == val)
+
+
+# Example
+ages = {
+  'Peter': 10,
+  'Isabel': 11,
+  'Anna': 10,
+}
+
+find_keys(ages, 10) # [ 'Peter', 'Anna' ]
