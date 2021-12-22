7729ffe844fda95f5b0ddd6151e9b29bb565ff56
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-22 20:55:52 +0900

    Create [List, Slice] head.py

---
+++ 30_seconds_of_code/[List, Slice] head.py
@@ -0,0 +1,15 @@
+'''
+head
+Returns the head of a list
+
+- Use lst[0] to return the first element of the passed list
+
+https://www.30secondsofcode.org/python/s/head
+'''
+
+def head(lst):
+    return lst[0]
+
+
+# Example
+head([1, 2, 3]) # 1
