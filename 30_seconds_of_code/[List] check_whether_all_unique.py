c5b58451af991fad77cf91a1bf4575fa3f316ee0
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-22 20:54:41 +0900

    Update [List] check_whether_all_unique.py

--- 30_seconds_of_code/[List] check_whether_all_unique.py
+++ 30_seconds_of_code/[List] check_whether_all_unique.py
@@ -1,6 +1,6 @@
 
 '''
-all_unique
+1) all_unique
 Checks if all the values in a list are unique
 
 - Use set() on the given list to keep only unique occurrences
@@ -18,4 +18,25 @@ x = [1, 2, 3, 4, 5, 6]
 y = [1, 2, 2, 3, 4, 5]
 all_unique(x) # True
 all_unique(y) # False
+
+
+'''
+2) has_duplicates
+Checks if there are duplicate values in a flat list
+
+- Use set() on the given list to remove duplicates,
+  compare its length with the length of the list
+
+https://www.30secondsofcode.org/python/s/has-duplicates
+'''
+
+def has_duplicates(lst):
+    return len(lst) != len(set(lst))
+
+
+# Example
+x = [1, 2, 3, 4, 5, 5]
+y = [1, 2, 3, 4, 5]
+has_duplicates(x) # True
+has_duplicates(y) # False
 
