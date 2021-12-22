0ef4fb225c3c51c6d89dffcb64c7c40d5df742eb
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-22 20:52:55 +0900

    Create [List] count_value_occurrence.py

---
+++ 30_seconds_of_code/[List] count_value_occurrence.py
@@ -0,0 +1,16 @@
+
+'''
+count_occurrences
+Counts the occurrences of a value in a list
+
+- Use list.count() to count the number of occurrences of val in lst
+
+https://www.30secondsofcode.org/python/s/count-occurrences
+'''
+
+def count_occurrences(lst, val):
+    return lst.count(val)
+
+
+# Example
+count_occurrences([1, 1, 2, 1, 2, 3], 1) # 3
