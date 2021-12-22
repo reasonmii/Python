c794ee4c833a20afcd423f74c212781134220675
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-22 20:21:22 +0900

    Create [List] check_whether_all_unique.py

---
+++ 30_seconds_of_code/[List] check_whether_all_unique.py
@@ -0,0 +1,21 @@
+
+'''
+all_unique
+Checks if all the values in a list are unique
+
+- Use set() on the given list to keep only unique occurrences
+- Use len() to compare the length of the unique values to the original list
+
+https://www.30secondsofcode.org/python/s/all-unique
+'''
+
+def all_unique(lst):
+    return len(lst) == len(set(lst))
+
+
+# Example
+x = [1, 2, 3, 4, 5, 6]
+y = [1, 2, 2, 3, 4, 5]
+all_unique(x) # True
+all_unique(y) # False
+
