70a4d6ce2cf78f8184e94353cb853370deda0090
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 09:14:49 +0900

    Update and rename [List, Slice] head.py to [List, Slice] head_or_last.py

---
+++ 30_seconds_of_code/[List, Slice] head_or_last.py
@@ -0,0 +1,32 @@
+'''
+1) head
+Returns the head of a list
+
+- Use lst[0] to return the first element of the passed list
+
+https://www.30secondsofcode.org/python/s/head
+'''
+
+def head(lst):
+    return lst[0]
+
+
+# Example
+head([1, 2, 3]) # 1
+
+
+'''
+2) last
+Returns the last element in a list
+
+- Use lst[-1] to return the last element of the passed list
+
+https://www.30secondsofcode.org/python/s/last
+'''
+
+def last(lst):
+    return lst[-1]
+
+
+# Example
+last([1, 2, 3]) # 3
