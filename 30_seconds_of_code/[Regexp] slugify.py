9572b8cb8f2bd535f0c0303f31863968d29bd25e
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 23:34:05 +0900

    Update [Regexp] slugify.py

--- 30_seconds_of_code/[Regexp] slugify.py
+++ 30_seconds_of_code/[Regexp] slugify.py
@@ -1,6 +1,6 @@
 
 '''
-slugify
+1) slugify
 Converts a string to a URL-friendly slug
 
 - Use str.lower() and str.strip() to normalize the input string
@@ -40,7 +40,51 @@ slugify('Hello World!')           # 'hello-world'
 slugify('___This is a test ---')  # 'this-is-a-test'
 
 
+'''
+2) kebab
+Converts a string to kebab case
+
+- Use re.sub() to replace any - or _ with a space, using the regexp r"(_|-)+"
+- Use re.sub() to match all words in the string, str.lower() to lowercase them
+- Finally, use str.join() to combine all word using - as the separator
+
+정규표현식 Regular Expression
+https://wikidocs.net/4308
+1) \s
+   whitespace 문자와 매치
+   [ \t\n\r\f\v]와 동일한 표현식
+   맨 앞의 빈 칸은 공백문자(space)를 의미
+2) ? : 0~1회 일치
+3) 반복 (+) : 1번 이상
+4) 반복 (*) : 0번 이상
+5) 반복 ({m,n}, ?)
+   {1,}은 +와 동일, {0,}은 *와 동일
+   ex) ca{2}t : a 반드시 두 번 반복
+   ex) ca{2,5}t : a 2~5번 반복
+
+https://www.30secondsofcode.org/python/s/kebab
+'''
+
+from re import sub
+
+def kebab(s):
+    return '-'.join(
+        sub(r'(\s|_|-)+'," ",
+            sub(r'[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+',
+                lambda mo: ' ' + mo.group(0).lower(), s)).split())
+  
+
+# Example
+kebab('camelCase') # 'camel-case'
+kebab('some text') # 'some-text'
+kebab('some-mixed_string With spaces_underscores-and-hyphens')
+# 'some-mixed-string-with-spaces-underscores-and-hyphens'
+kebab('AllThe-small Things') # 'all-the-small-things'
+
+
 # reference =====================================================
+
+### re.sub（정규 표현식, 대상 문자열 , 치환 문자
 
 s = '-hello_world-tick*$^tock--123+-'
 
@@ -54,3 +98,19 @@ sub(r'[\s_-]+', '-', s)
 
 sub(r'^-+|-+$', '', s)
 # 'hello_world-tick*$^tock--123+'
+
+import re
+text = "I like apble And abple"
+text_mod = re.sub('apble|abple',"apple",text)
+print (text_mod)  # I like apple And apple
+
+### 문자열 앞에 r이 붙으면 해당 문자열이 구성된 그대로 문자열로 반환
+a = 'abcdef\n'
+print(a)   # abcdef
+
+b = r'abcdef\n'
+print(b)   # abcdef\n
+
+###
+sub(r'(\s|_|-)+'," ","abc-def_ghi jkl")   # 'abc def ghi jkl'
+
