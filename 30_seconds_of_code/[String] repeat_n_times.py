004cca28d1fef4616e736cdd19e211161e8ce0e5
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 09:35:09 +0900

    Create [String] repeat_n_times.py

---
+++ 30_seconds_of_code/[String] repeat_n_times.py
@@ -0,0 +1,16 @@
+
+'''
+n_times_string
+Generates a string with the given string value repeated n number of times
+
+- Repeat the string n times, using the * operator
+
+https://www.30secondsofcode.org/python/s/n-times-string
+'''
+
+def n_times_string(s, n):
+    return (s * n)
+
+
+# Example
+n_times_string('py', 4) #'pypypypy'
