6421cd3c933e35b2e2930dfd5a94f63d9f6ec7a0
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 09:33:52 +0900

    Create [String] capitalize_every_word.py

---
+++ 30_seconds_of_code/[String] capitalize_every_word.py
@@ -0,0 +1,16 @@
+
+'''
+capitalize_every_word
+Capitalizes the first letter of every word in a string
+
+- Use str.title() to capitalize the first letter of every word in the string
+
+https://www.30secondsofcode.org/python/s/capitalize-every-word
+'''
+
+def capitalize_every_word(s):
+    return s.title()
+
+
+# Example
+capitalize_every_word('hello world!')  # 'Hello World!'
