fd076da4f2c5e4915fb05099e0d2242f874ef95c
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 09:00:20 +0900

    Update [List, Intersection] any_value_without_duplicate.py

--- 30_seconds_of_code/[List, Intersection] any_value_without_duplicate.py
+++ 30_seconds_of_code/[List, Intersection] any_value_without_duplicate.py
@@ -1,3 +1,22 @@
+'''
+intersection
+Returns a list of elements that exist in both lists
+
+- Create a set from a and b
+- Use the built-in set operator & to only keep values contained in both sets,
+  then transform the set back into a list
+
+https://www.30secondsofcode.org/python/s/intersection
+'''
+
+def intersection(a, b):
+    _a, _b = set(a), set(b)
+    return list(_a & _b)
+
+
+# Example
+intersection([1, 2, 3], [4, 3, 2]) # [2, 3]
+
 
 '''
 union_by
