9b377c416dd74788a2358e77e245408b218e9e60
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 23:26:22 +0900

    Update [List] values_in_another_list.py

--- 30_seconds_of_code/[List] values_in_another_list.py
+++ 30_seconds_of_code/[List] values_in_another_list.py
@@ -1,5 +1,26 @@
 '''
-1) includes_all
+1) is_contained_in
+Checks if the elements of the first list are contained in the second one regardless of order
+
+- Use count() to check if any value in a has more occurrences than it has in b
+- Return False if any such value is found, True otherwise
+
+https://www.30secondsofcode.org/python/s/is-contained-in
+'''
+
+def is_contained_in(a, b):
+  for v in set(a):
+    if a.count(v) > b.count(v)
+    return False
+  return True
+
+
+# Example
+is_contained_in([1, 4], [2, 4, 1]) # True
+
+
+'''
+2) includes_all
 Checks if all the elements in values are included in lst
 
 - Check if every value in values is contained in lst using a for loop
@@ -21,7 +42,7 @@ includes_all([1, 2, 3, 4], [1, 5]) # False
 
 
 '''
-2) includes_any
+3) includes_any
 Checks if any element in values is included in lst
 
 - Check if any value in values is contained in lst using a for loop
