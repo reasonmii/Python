233170353047cc2b4bfc58c6193b897bfca439e2
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 23:02:20 +0900

    Update [List, Index, next] find_index_match_func.py

--- 30_seconds_of_code/[List, Index, next] find_index_match_func.py
+++ 30_seconds_of_code/[List, Index, next] find_index_match_func.py
@@ -37,6 +37,25 @@ def find_last_index(lst, fn):
 find_last_index([1, 2, 3, 4], lambda n: n % 2 == 1) # 2
 
 
+'''
+3) find_index_of_all
+Finds the indexes of all elements in the given list
+that satisfy the provided testing function
+
+- Use enumerate() and a list comprehension to return the indexes
+  of the all element in lst for which fn returns True
+
+https://www.30secondsofcode.org/python/s/find-index-of-all
+'''
+
+def find_index_of_all(lst, fn):
+    return [idx for idx, x in enumerate(lst) if fn(x)]
+
+
+# Example
+find_index_of_all([1, 2, 3, 4], lambda n: n % 2 == 1) # [0, 2]
+
+
 # reference =====================================================
 # iter는 객체의 __iter__ method 호출
 # next는 객체의 __next__ method 호출
