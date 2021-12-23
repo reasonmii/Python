8fb7dad3588c9f5b2f2cdc1b47bbbb13e96dfd75
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 22:59:53 +0900

    Update [List, Filter] unique_values_with_counter.py

--- 30_seconds_of_code/[List, Filter] unique_values_with_counter.py
+++ 30_seconds_of_code/[List, Filter] unique_values_with_counter.py
@@ -1,6 +1,26 @@
 
 '''
-filter_non_unique
+1) filter_unique
+Creates a list with the unique values filtered out
+
+- Use collections.Counter to get the count of each value in the list.
+- Use a list comprehension to create a list containing only the non-unique values.
+
+https://www.30secondsofcode.org/python/s/filter-unique
+'''
+
+from collections import Counter
+
+def filter_unique(lst):
+    return [item for item, count in Counter(lst).items() if count > 1]
+
+
+# Example
+filter_unique([1, 2, 2, 3, 4, 4, 5]) # [2, 4]
+
+
+'''
+2) filter_non_unique
 Creates a list with the non-unique values filtered out
 
 - Use collections.Counter to get the count of each value in the list
@@ -33,4 +53,3 @@ for item, count in Counter([1, 2, 2, 3, 4, 4, 5]).items():
 # 3 1
 # 4 2
 # 5 1
-
