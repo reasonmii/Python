575ca02bccc3313fba67cef690cf8cda67c6f5cb
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 23:12:53 +0900

    Update [List] difference_between_lists.py

--- 30_seconds_of_code/[List] difference_between_lists.py
+++ 30_seconds_of_code/[List] difference_between_lists.py
@@ -1,6 +1,6 @@
 
 '''
-difference
+1) difference
 Calculates the difference between two iterables, without filtering duplicate values
 
 - Create a set from b
@@ -18,3 +18,23 @@ def difference(a, b):
 # Example
 difference([1, 2, 3], [1, 2, 4]) # [3]
 
+
+'''
+2) symmetric_difference
+Returns the symmetric difference between two iterables,
+without filtering out duplicate values
+
+- Create a set from each list
+- Use a list comprehension on each of them to only keep values
+  not contained in the previously created set of the other
+
+https://www.30secondsofcode.org/python/s/symmetric-difference
+'''
+
+def symmetric_difference(a, b):
+    (_a, _b) = (set(a), set(b))
+    return [item for item in a if item not in _b] + [item for item in b if item not in _a]
+  
+
+# Example
+symmetric_difference([1, 2, 3], [1, 2, 4]) # [3, 4]
