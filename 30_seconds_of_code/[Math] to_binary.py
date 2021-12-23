2a29597577d135dd46e744c2b4b76ec2326d8f88
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-12-23 11:10:08 +0900

    Create [Math] to_binary.py

---
+++ 30_seconds_of_code/[Math] to_binary.py
@@ -0,0 +1,16 @@
+
+'''
+to_binary
+Returns the binary representation of the given number
+
+- Use bin() to convert a given decimal number into its binary equivalent
+
+https://www.30secondsofcode.org/python/s/to-binary
+'''
+
+def to_binary(n):
+    return bin(n)
+
+
+# Example
+to_binary(100) # 0b1100100
